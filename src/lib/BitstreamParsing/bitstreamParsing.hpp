8296f08c1c7e7e510a2b620bab9f471f483fa832
Heikki <heikki.tampio@tuni.fi>
2024-07-15 11:27:54 +0300

    Implement V3C sample stream traversal

--- src/lib/BitstreamParsing/bitstreamParsing.hpp
+++ src/lib/BitstreamParsing/bitstreamParsing.hpp
@@ -2,9 +2,15 @@
 
 #include "uvgvpccdec/uvgvpccdec.hpp"
 
+struct bitstream_position {
+  uint64_t bytes = 0;
+  uint8_t  bits = 0;
+};
 
 class BitstreamParsing {
-   public:
+public: 
+    static uint32_t read(uint8_t bits);
+    static void advance_bitstream(std::size_t bits);
     static void initializeStaticParameters(const uvgvpcc_dec::Parameters& param);
     static void parseV3CSampleStream(const std::vector<uint8_t> &data);
 };
